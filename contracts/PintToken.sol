76189947cecc2c33e8694c93c817ebad52129366
Danilo D'Alessio <ciccio@AirdiMacbookair.lan>
2018-04-16 14:08:17 +0200

    3rd Commit

--- contracts/PintToken.sol
+++ contracts/PintToken.sol
@@ -6,6 +6,12 @@ contract PintToken {
     string public symbol = "PNT";
     string public standard = "PNT Token v.1.0";
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+      );
+
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
@@ -13,5 +19,14 @@ contract PintToken {
     function PintToken (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
