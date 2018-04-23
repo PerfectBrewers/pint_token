51f1afe3af4178018dbd16b196cd489dd4cb98cb
Danilo D'Alessio <ciccio@AirdiMacbookair.lan>
2018-04-23 14:30:32 +0200

    Token Sale 2

--- test/PintTokenSale.js
+++ test/PintTokenSale.js
@@ -2,7 +2,9 @@ var PintTokenSale = artifacts.require("./PintTokenSale.sol");
 
 contract('PintTokenSale', function(accounts) {
   var tokenSaleInstance;
+  var buyer = accounts[0];
   var tokenPrice = 1000000000000000; // in wei
+  var numberOfTokens;
 
   it('initializes the contract with the correct values', function() {
     return PintTokenSale.deployed().then(function(instance) {
@@ -16,6 +18,18 @@ contract('PintTokenSale', function(accounts) {
     return tokenSaleInstance.tokenPrice();
     }).then(function(price) {
     assert.equal(price, tokenPrice, 'token price is correct');
+    });
+  });
+
+  it('facilitates token buying', function() {
+    return PintTokenSale.deployed().then(function(instance) {
+      tokenSaleInstance = instance;
+      numberOfTokens = 10;
+      return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice })
+    }).then(function(receipt) {
+      return tokenSaleInstance.tokenSold();
+    }).then(function(amount) {
+      assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of token sold');
     });
   });
 });
